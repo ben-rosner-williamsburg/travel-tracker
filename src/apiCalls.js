52422818ac176f4e0557bad97acc57f879ec6d4c
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-11-08 11:58:39 -0500

    Add fetch promises function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,19 @@
-export const travelers = "http://localhost:3001/api/v1/travelers"
-export const singleTraveler = `http://localhost:3001/api/v1/travelers/${id}`
-export const trips = "http://localhost:3001/api/v1/trips"
-export const destinations = "http://localhost:3001/api/v1/destinations"
+export const travelers = "http://localhost:3001/api/v1/travelers";
+export const singleTraveler = `http://localhost:3001/api/v1/travelers/${id}`;
+export const trips = "http://localhost:3001/api/v1/trips";
+export const destinations = "http://localhost:3001/api/v1/destinations";
 
-const endpoints = [travelers, singleTraveler, trips, destinations]
+const endpoints = [travelers, singleTraveler, trips, destinations];
+
+export const fetchPromises = endpoints.map(url => {
+  fetch(url).then((response) => {
+     if (!response.ok) {
+      throw new Error(`${response.status} : "Failed to fetch data`);
+     }
+     return response.json();
+  }).then((data) => {
+      return data;
+  }).catch(error => {
+      console.error(error);
+  })
+})
