be10cf87851af9b16fb68bbc64829a050ebca337
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-11-08 11:20:38 -0500

    Add endpoint links and endpoint array

---
+++ src/apiCalls.js
@@ -0,0 +1,6 @@
+export const travelers = "http://localhost:3001/api/v1/travelers"
+export const singleTraveler = `http://localhost:3001/api/v1/travelers/${id}`
+export const trips = "http://localhost:3001/api/v1/trips"
+export const destinations = "http://localhost:3001/api/v1/destinations"
+
+const endpoints = [travelers, singleTraveler, trips, destinations]
