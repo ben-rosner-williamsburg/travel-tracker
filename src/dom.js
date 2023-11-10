758ec807684a62a6f4e2f5a42aedfbb9686139a9
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-11-10 10:22:37 -0500

    Add total costs to DOM

--- src/dom.js
+++ src/dom.js
@@ -3,8 +3,9 @@ const tripsField = document.querySelector('.trips');
 const locationField = document.querySelector('.location')
 const moneySpentField = document.querySelector(".money-spent");
 
-export const displayUserTrips = (selectedUser, trips, location) => {
-  nameField.innerText = `${selectedUser.name}`;
-  tripsField.innerText = `${trips}`
-  locationField.innerText = `${location} `
+export const displayUserTrips = (selectedUser, trips, location, totalCost) => {
+  nameField.innerText = `Welcome ${selectedUser.name}!`;
+  tripsField.innerText = `Trip Dates: ${trips}`
+  locationField.innerText = `Location of Trips: ${location}`
+  moneySpentField.innerText = `Total Cost of Trips: $${totalCost}`
 }
--- src/scripts.js
+++ src/scripts.js
@@ -7,7 +7,7 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import { fetchPromises } from './apiCalls';
-import { getRandomTraveler, getDataByIndex, filterData, getTripDates, getDestinationIDs, findDestination, getLodgingCost, getFlightCost } from './dataModel';
+import { getRandomTraveler, getDataByIndex, filterData, getTripDates, getDestinationIDs, findDestination, getLodgingCost, getFlightCost, getTotalCost } from './dataModel';
 import { displayUserTrips } from './dom';
 
 
